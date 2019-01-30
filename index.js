e470a0653b06ca7b36d0084467b7956bfb87a55d
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 18:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 function destructivelyAppendKitten(kittens,"Ralph"){
-kittens.push("Ralps");
+kittens.push("Ralph");
 return kittens
 }
 
