69a85c74ef0ae0a86542bc52f2dd160b7c82a4de
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 18:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
+function destructivelyAppendKitten(kittens){
+kittens.unshift("Ralps");
+return kittens  
+}
+
 // Add your functions and code here
